ef71c9f9615e844a78f2c66efb2771feb63c0ba4
saitarun1806 <kunanisai123@gmail.com>
2026-06-28 23:18:16 +0530

    client add

--- server/src/routes/linkRoutes.js
+++ server/src/routes/linkRoutes.js
@@ -37,5 +37,93 @@ router.post("/shorten",authMiddleware, async (req, res) => {
     }
 });
 
+router.get("/links",authMiddleware, async(req,res)=>{
+    try{
+        const links = await Link.find({
+            userId:req.userId
+        }).sort({createdAt:-1});
+        res.status(200).json({
+            success:true,
+            count:links.length,
+            links
+                
+        });
+    }catch(err){
+        res.status(404).json({message:err.message});
+    }
+});
+
+router.get("/links/:id",authMiddleware,async(req,res)=>{
+    try{
+        const link = await Link.findOne({
+            _id:req.params.id,
+            userId:req.userId
+        });
+        if(!link){
+            return res.status(404).json({
+                message:"Link Not Found"
+            });
+        }
+        res.status(200).json(link);
+    }catch(err){
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+router.put("/links/:id",authMiddleware,async(req,res)=>{
+    try{
+        const {originalUrl}=req.body;
+        const link= await Link.findOneAndUpdate(
+            {
+                _id:req.params.id,
+                userId:req.userId
+            },
+            {
+                originalUrl
+            },
+            {
+                new:true
+            }
+        );
+        if(!link){
+            return res.status(404).json({
+                message:"Link Not Found"
+            });
+        }
+        res.status(200).json({
+            success: true,
+            link
+        });
+    }catch(err){
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
+
+router.delete("/links/:id",authMiddleware,async(req,res)=>{
+    try{
+        const link = await Link.findOneAndDelete({
+            _id:req.params.id,
+            userId:req.userId
+        });
+        if(!link){
+            return res.status(404).json({
+                message:"Link not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"link deleted successfully"
+        });
+    }catch(err){
+        res.status(500).json({
+            message:err.message
+        });
+    }
+});
+
 
 export default router;
